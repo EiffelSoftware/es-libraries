9365434e5e0f36a59915aae4bcae185e30c296d3
Jocelyn Fiat <git@djoce.net>
2024-09-20 14:58:22 +0200

    Converted unix deliv scripts to support git repository as main EiffelStudio source code repository.

--- vision2/widget_test/retrieve_build_files.bat
+++ vision2/widget_test/retrieve_build_files.bat
@@ -1,30 +1,30 @@
 @echo off
 if .%1. == .. (
-set SVN_URL=https://svn.origo.ethz.ch/eiffelstudio/trunk
+set DELIV_DIR=%EIFFEL_SRC%\Delivery
 ) else (
-set SVN_URL=%*
+set DELIV_DIR=%1
 )
 
 REM Check out bitmaps from Build delivery
-call svn_command.bat co %SVN_URL%/Src/Delivery/esbuilder/bitmaps Delivery\esbuilder\bitmaps
+XCOPY /Y /E /I  %DELIV_DIR%\esbuilder\bitmaps _Delivery_esbuilder_bitmaps
 
 REM Check out files from vision2_demo
-call svn_command.bat co %SVN_URL%/Src/Delivery/vision2_demo Delivery/vision2_demo
+XCOPY /Y /E /I  %DELIV_DIR%\vision2_demo _Delivery_vision2_demo
 
 REM Copy template files
-XCOPY /Y /E /I Delivery\vision2_demo\templates .\templates
+XCOPY /Y /E /I _Delivery_vision2_demo\templates .\templates
 
 REM Copy icons for different widget types across. The /E option moves the whole directory structure.
-XCOPY /Y /E /I Delivery\esbuilder\bitmaps .\bitmaps
+XCOPY /Y /E /I _Delivery_esbuilder_bitmaps .\bitmaps
 
 REM Copy icons for standard buttons.
-XCOPY /Y Delivery\vision2_demo\bitmaps\ico\documentation.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_demo\bitmaps\ico\testing.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_demo\bitmaps\ico\properties.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_demo\bitmaps\ico\size_down.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_demo\bitmaps\ico\size_up.ico .\bitmaps\ico\
-XCOPY /Y Delivery\vision2_demo\bitmaps\png\image1.png .\bitmaps\png\
-XCOPY /Y Delivery\vision2_demo\bitmaps\png\image2.png .\bitmaps\png\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\ico\documentation.ico .\bitmaps\ico\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\ico\testing.ico .\bitmaps\ico\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\ico\properties.ico .\bitmaps\ico\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\ico\size_down.ico .\bitmaps\ico\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\ico\size_up.ico .\bitmaps\ico\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\png\image1.png .\bitmaps\png\
+XCOPY /Y _Delivery\vision2_demo\bitmaps\png\image2.png .\bitmaps\png\
 
 REM Copy image used for executable icon.
 
@@ -33,4 +33,4 @@ ren default_vision2_icon.ico vision2_demo.ico
 
 
 REM Remove all temporary checked out files.
-rd /Q /S Delivery
+rd /Q /S _Delivery
